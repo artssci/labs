40c5ec2ae81d9c3693f599a10ffcd9a3a256db2c
luis <luisnavarrodelangel@gmail.com>
2026-01-07 15:11:07 -0500

    updates

---
+++ exercise1.js
@@ -0,0 +1,11 @@
+
+const name = "Luis";
+const role = "student";
+const studentNumber = 123456;
+const isInquiryCourse = true;
+
+
+console.log(name)
+
+
+
---
+++ exercise2.js
@@ -0,0 +1,20 @@
+
+// Step 1: Variables as Labels
+let name = "Alex";
+let role = "Student";
+let studentNumber = 123456;
+let isInquiryCourse = true;
+
+console.log(role)
+
+let participationGrade = 85;
+let threshold = 80; 
+
+let result = participationGrade > threshold ? "Excellent" : "Standard"
+
+console.log(result);
+
+
+let finalMessage = name + "is a " + role + " with status: " + result;
+
+console.log(finalMessage);
